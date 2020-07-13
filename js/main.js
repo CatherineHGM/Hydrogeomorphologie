023c6952b3d79d3a583591e3d050f9f325307ee2
CatherineHGM <68040334+CatherineHGM@users.noreply.github.com>
2020-07-13 11:21:40 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //ajouton tous notre code a linterieur de window.onload car le js file a tendance a se loader plus vite que le html ce qui fait
 //qui ne selectionnera pas nos element comme getelementbyid et tout.
-
+// permet dacceder aux differentes sections(hydrologie, geomorphologie, etc.) + rapidement
 window.onload= function (){
    //jquery smooth scrolling
 $('#navbar a').on('click', function (event) {
@@ -15,6 +15,7 @@ $('#navbar a').on('click', function (event) {
     }
 });
 
+//2e page du site web pour developper la petite carte en une page 
 document.getElementById('developper').addEventListener('click', function() {
     document.querySelector('.bg-modal').style.display = 'flex';
 })
